61e5c92ffd9ae771aa04382fde4d7dfaf96f3d1a
Rut <rut.abello@gmail.com>
2020-02-04 00:58:09 +0100

    added an npm component for countdown in .json. Created 4 buttons (component)

---
+++ music-app/src/Components/QuestionsPage/roundTimer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const MyCountdown = (props) => {
+    
+    const renderTime = value => {
+        if (value === 0) {
+          return <div className="timer">Too late...</div>;
+        }
+      
+        return (
+          <div className="timer">
+            <div className="text">Remaining</div>
+            <div className="value">{value}</div>
+            <div className="text">seconds</div>
+          </div>
+        );
+    };
+    
+    
+    return (
+        <CountdownCircleTimer
+        isPlaying
+        durationSeconds={10}
+        colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+        renderTime={renderTime}
+        // onComplete={() => [true, 1000]}
+      />
+    )
+};
+
+export default MyCountdown;
