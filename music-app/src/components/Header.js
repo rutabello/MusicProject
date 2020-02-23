3deba45be47d1466c4d25c2958bc366d4d545620
AlundraOz <ailuozols@gmail.com>
2020-02-23 09:42:54 +0100

    added button w/sound and background music

---
+++ music-app/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+import Button from './Button.js'
+
+function Header() {
+    return (
+        <div className="main">
+            <span className="webdev">Play  
+            <span>&nbsp;Some</span>
+            <span>&nbsp;With</span>
+            </span>
+            <span className="socod">Music</span>
+            <Button />
+        </div>
+         
+    )
+}
+
+export default Header
